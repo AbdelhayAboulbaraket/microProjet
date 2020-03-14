631e2cd5245711f7af97ce6b3658c38f029d1b89
BoogeyIrl <boogeyIrl@users.noreply.github.com>
2020-03-14 21:54:41 +0100

    a

--- src/myDb/DBConnect.java
+++ src/myDb/DBConnect.java
@@ -46,7 +46,7 @@ public class DBConnect {
 						"CREATE TABLE client( id INT(10) AUTO_INCREMENT, email  VARCHAR(50), nom VARCHAR(30), prenom VARCHAR(30), adresse VARCHAR(30),code_postal VARCHAR(30),ville VARCHAR(30),tel VARCHAR(30),mot_de_passe VARCHAR(100), PRIMARY KEY(id));" + 
 						"CREATE TABLE commande( num_commande INT(10) AUTO_INCREMENT, id_client INT(10),date_commande DATE, PRIMARY KEY(num_commande), FOREIGN KEY(id_client) REFERENCES client(id) );" +
 						"CREATE TABLE categorie(ref_cat INT(10),cat VARCHAR(30),PRIMARY KEY(ref_cat));" + 
-						"CREATE TABLE article(code_article INT(10) AUTO_INCREMENT,designation VARCHAR(100),prix FLOAT,stock INT(10),categorie INT(10),image BLOB,PRIMARY KEY(code_article),FOREIGN KEY(categorie) REFERENCS categorie(ref_cat));" +
+						"CREATE TABLE article(code_article INT(10) AUTO_INCREMENT,designation VARCHAR(100),prix FLOAT,stock INT(10),categorie INT(10),image BLOB,PRIMARY KEY(code_article),FOREIGN KEY(categorie) REFERENCES categorie(ref_cat));" +
 						"CREATE TABLE lignes_commande(num_commande INT(10),code_article INT(10),qte_cde INT(10),FOREIGN KEY(num_commande) REFERENCES commande(num_commande),FOREIGN KEY(code_article) REFERENCES article(code_article));";
 				req.executeUpdate(sql);
 				System.out.println("DB crée");
